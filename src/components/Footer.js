df87617c87b03fed7ceea16733f148cbccbfeb4b
Elvio Galeano <elvio.galeano827@gmail.com>
2022-12-26 20:54:34 -0300

    fix red social

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,17 @@ export default function Footer() {
         <span className='logo'><span className='color'>A</span>mazing <span className='color'>E</span>vents</span>
         </div>
         <div className='footer-red-social'>
-            <img className='red-social' alt='logo instagram' src='https://cdn-icons-png.flaticon.com/512/2111/2111463.png' />
+          <a href='https://youtube.com' target="_blank">
             <img className='red-social' alt='logo youtube' src='https://cdn-icons-png.flaticon.com/512/3938/3938037.png' />
+          </a>
+          <a href='https://instagram.com' target="_blank">
+            <img className='red-social' alt='logo instagram' src='https://cdn-icons-png.flaticon.com/512/2111/2111463.png' />
+          </a>
+          <a href='https://twitter.com' target="_blank">
             <img className='red-social' alt='logo twitter' src='https://cdn-icons-png.flaticon.com/512/2504/2504947.png' />
+          </a>  
+            
+            
         </div>
         <div>
             <img className='red-social top' alt='flecha' src='https://cdn-icons-png.flaticon.com/512/992/992703.png' onClick={scrollTo}/>
